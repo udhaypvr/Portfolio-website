3f833d441cb364b22d892582c19258752950f2e2
udhay <udhaypvr@gmail.com>
2025-07-03 12:49:09 +0530

    change-2.1

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -38,7 +38,7 @@ export function About() {
           <div className="md:col-span-1 flex justify-center">
             <FadeIn delay="0.2s">
               <Image
-                src="/path/to/udhay.jpg" // REMINDER: Update this path to your actual image
+                src="/udhay.jpeg" 
                 alt="Portrait of Udhay, the backend developer"
                 width={300}
                 height={300}
